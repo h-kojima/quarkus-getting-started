e062acf83dd6fc68bcb868ea5a7d9d1764484a15
Hirofumi Kojima <hkojima@redhat.com>
2023-12-16 14:41:42 +0900

    updated

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -22,6 +22,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello!! YOUR_NAME!!!!";
+        return "hello!! <YOUR_NAME>!!!!";
     }
 }
